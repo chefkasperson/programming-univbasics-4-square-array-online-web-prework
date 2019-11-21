c5592ae3e0c71bf8905edb615b8dff81e0c15f05
Erik Kasperson <chefkasperson@gmail.com>
2019-11-21 08:11:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,8 @@ def square_array(array)
   counter = 0
   
   while counter < array.length do
-    new_array << array[counter]
+    new_array << array[counter] ** 2
     counter += 1
+    return new_array
 end
 end
