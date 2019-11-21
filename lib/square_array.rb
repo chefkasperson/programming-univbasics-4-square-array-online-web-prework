ebf4d500b5db810c979a204825eaa5c3ff415e6e
Erik Kasperson <chefkasperson@gmail.com>
2019-11-21 07:55:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,11 @@
+#while as the iterator
+#take an array of numbers, square each number, return a new array with squared numbers
+
+
 def square_array(array)
+ new_array = []
  counter = 0 
-  while array[counter] do
-    array[counter]**2 
+  while counter < array.length do
+    new_array << array[counter] ** 2 
     counter += 1 
 end
